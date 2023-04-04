ee7b08964acac653de0dcf6bfc2f2d4f64ea43c5
Jude Sajith <judesajith.aj@gmail.com>
2023-04-04 20:17:43 +0530

    ssss

--- pages/Query.py
+++ pages/Query.py
@@ -32,55 +32,24 @@ toc = st.button("Get TOC")
 if toc:
     toc_res = index.query(f"Generate a full table of contents for this book in a json format ")
     str_toc = str(toc_res)
-    st.write(str_toc)
+    # st.write(str_toc)
     json_output = json.loads(str_toc)
     table_of_contents = json_output["Table of Contents"]
+    if "table_of_contents" not in st.session_state:
+        st.session_state.table_of_contents = table_of_contents
 
+
+
+try:
     selected_items = []
-    for item in table_of_contents:
+    for item in st.session_state.table_of_contents:
         for title, content in item.items():
             if st.checkbox(title):
                 selected_items.append(content)
 
     if selected_items:
         st.write("Selected items:", selected_items)
-
-    # if "json_output" not in st.session_state:
-    #     st.session_state.json_output = json_output
-
-
-# col1, col2, col3 = st.columns(3)
-
-
-# selected_item = col1.radio("Select an item:", json_output)
-
-# if selected_item:
-#     # loprompt= f"list down the contents under the Learning Objectives of the chapter {selected_item} of this book as a json list"
-#     toprompt =f"list down the contents inside the topic {selected_item} of this book as a json list"
-#     # lores = index.query(loprompt)
-#     # str_lo = str(lores)
-#     # json_lo = json.loads(str_lo)
-   
-
-#     topires = index.query(toprompt)
-#     # str_topi = str(topires)
-#     # json_topi = json.loads(str_topi)
-   
-
-#     # with col2.expander("Learning Objectives"):
-#     # col2.write(loprompt)
-#     # col2.write(json_lo)
-
-# # with col3.expander("topics"):
-#     col3.write(toprompt)
-#     col3.write(topires)
-
+except ValueError:
+    st.warning("Generate TOC to view list")  
 else:
     st.warning("Click the 'Chapters' button to retrieve the table of contents.")
-
-
-# with st.expander("Chapters"):
-#     st.write(st.session_state.json_output)
-
-
-
