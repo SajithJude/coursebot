098522364875e56b1873bb12bc8b44850e4fbfa9
Jude Sajith <judesajith.aj@gmail.com>
2023-04-05 05:00:30 +0530

    sssss

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ try:
 
     st.write("")
     # st.write()
-    col1, col2, col3 = st.tabs(["__Filter Table of Contents__  ", " __Extract and Edit Content__  ","  __Export Generated XML__  "])
+    col1, col2, col3 = st.tabs(["🔵__Filter Table of Contents__  ", "🔵 __Extract and Edit Content__  "," 🔵 __Export Generated XML__  "])
 
     if "selected_items" not in st.session_state:
         st.session_state.selected_items = []
