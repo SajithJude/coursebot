7181f5e2566a704691e0f4d5871d3589404f8086
Jude Sajith <judesajith.aj@gmail.com>
2023-04-05 01:16:10 +0530

    ssss

--- pages/Query.py
+++ pages/Query.py
@@ -7,10 +7,7 @@ import json
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
-
 from langchain import OpenAI
-
-st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 openai.api_key = os.getenv("API_KEY")
 
@@ -23,7 +20,7 @@ if index_filenames:
     # If there are index files available, create a dropdown to select the index file to load
     index_file = st.selectbox("Select an index file to load:", index_filenames)
     index_path = os.path.join(DATA_DIR, index_file)
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=1024))
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=512))
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
 
     index = GPTSimpleVectorIndex.load_from_disk(index_path,service_context=service_context)
@@ -31,49 +28,54 @@ else:
     # If there are no index files available, prompt the user to upload a PDF file
     st.warning("No index files found. Please upload a PDF file to create an index.")
     
-try:
-    toc = st.button("Get TOC")
 
-    if toc:
-        toc_res = index.query(f"Generate a full table of contents for this book in a json format ")
-        str_toc = str(toc_res)
-        # st.write(str_toc)
-        json_output = json.loads(str_toc)
-        table_of_contents = json_output["Table of Contents"]
-        if "table_of_contents" not in st.session_state:
-            st.session_state.table_of_contents = table_of_contents
+toc = st.button("Get TOC")
 
-    col1, col2 = st.columns(2)
+if toc:
+    toc_res = index.query(f"Generate a full table of contents for this book in a json format ")
+    str_toc = str(toc_res)
+    # st.write(str_toc)
+    json_output = json.loads(str_toc)
+    table_of_contents = json_output["Table of Contents"]
+    if "table_of_contents" not in st.session_state:
+        st.session_state.table_of_contents = table_of_contents
 
-    if "selected_items" not in st.session_state:
-        st.session_state.selected_items = []
+col1, col2, col3 = st.columns(3)
 
-    for item in st.session_state.table_of_contents:
-        for title, content in item.items():
-            if col1.checkbox(title):
-                if title not in st.session_state.selected_items:
-                    st.session_state.selected_items.append(title)
+if "selected_items" not in st.session_state:
+    st.session_state.selected_items = []
 
-    if st.button("Query"):
-        chapter_contents = {}
-        for title in st.session_state.selected_items:
-            chapter_content = index.query(f"Extract the contents under the title {title}")
-            chapter_contents[title] = chapter_content.response
+for item in st.session_state.table_of_contents:
+    for title, content in item.items():
+        if col1.checkbox(title):
+            if title not in st.session_state.selected_items:
+                st.session_state.selected_items.append(title)
 
-        if chapter_contents:
-            st.session_state.selected_chapters = chapter_contents
-            root = ET.Element("topics")
-            for key, value in st.session_state.selected_chapters.items():
-                topic = ET.SubElement(root, "topic_name")
-                topic.text = key
-                contents = ET.SubElement(root, "topic_contents")
-                contents.text = value
+if st.button("Query"):
+    chapter_contents = {}
+    for title in st.session_state.selected_items:
+        chapter_content = index.query(f"Extract the contents under the title {title}")
+        chapter_contents[title] = chapter_content.response
+
+    if chapter_contents:
+        st.session_state.selected_chapters = chapter_contents
+        root = ET.Element("topics")
+        for key, value in st.session_state.selected_chapters.items():
+            topic = ET.SubElement(root, "topic_name")
+            topic.text = key
+            contents = ET.SubElement(root, "topic_contents")
+            contents.text = value
+
+        if st.button("Save XML"):
             xml_string = ET.tostring(root)
+            # Use minidom to pretty print the XML string
             pretty_xml = minidom.parseString(xml_string).toprettyxml()
-            col2.code(pretty_xml, language=None)
+            col3.write(pretty_xml)
 
-
-    if not st.session_state.selected_items:
-        st.warning("Please select some chapters to extract")
-except AttributeError:
-    st.warning("Click on Get TOC button to inititate the process")
+        for title, content in st.session_state.selected_chapters.items():
+            col2.write(f"Title: {title}")
+            content_key = f"{title}_content"
+            if content_key not in st.session_state:
+                st.session_state[content_key] = content
+            content_value = st.text_input(label="Content", value=st.session_state[content_key], key=content_key)
+           
