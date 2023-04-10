7cefa74a34d47f088914f835efe38f699c3c80ab
Jude Sajith <judesajith.aj@gmail.com>
2023-04-10 16:23:48 +0530

    ssss

---
+++ pages/subtopics.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from pathlib import Path
+from llama_index import download_loader
+from llama_index import GPTSimpleVectorIndex, Document, LLMPredictor, ServiceContext
+from tempfile import NamedTemporaryFile
+
+
+PDFReader = download_loader("PDFReader")
+
+if "index" not in st.session_state:
+    st.session_state.index = ""
+
+def process_pdf(uploaded_file):
+    loader = PDFReader()
+    with NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+        temp_file.write(uploaded_file.getvalue())
+        documents = loader.load_data(file=Path(temp_file.name))
+
+    index = GPTSimpleVectorIndex.from_documents(documents)
+    # st.session_state.index = index
+    return index
+        
+
+uploaded_pdf = st.file_uploader("Upload a PDF file", type=["pdf"])
+
+    # Process uploaded PDF and create index
+if uploaded_pdf is not None:
+    # if session_state.index is None:
+    st.write("Processing the PDF file and creating an index...")
+    st.session_state.index = process_pdf(uploaded_pdf)
+    st.write("Index created successfully!")
+
+
+button = st.button("Generate TOC")
+if button:
+    res = st.session_state.index.query("Generate a table of contents for this chapter in including topics and subtopics as a json object")
+    st.write(res.response)
