72a11633ec22a6b16c3a1007e5e350b3b8c07af3
Jude Sajith <judesajith.aj@gmail.com>
2023-04-10 09:31:07 +0530

    ssss

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ try:
         toc_res = index.query(f"Generate all titles inside this chapter in a json list format ")
         str_toc = str(toc_res)
         st.write(str_toc)
-        json_output = json.loads(str_toc)
+        json_output = json.loads({str_toc})
         table_of_contents = json_output["Table of Contents"]
         if "table_of_contents" not in st.session_state:
             st.session_state.table_of_contents = table_of_contents
