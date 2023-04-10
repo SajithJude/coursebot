1fd7f4f71ac80c35decfe2937fb412ad9e7a1533
Jude Sajith <judesajith.aj@gmail.com>
2023-04-10 16:52:05 +0530

    ssss

--- pages/subtopics.py
+++ pages/subtopics.py
@@ -47,7 +47,7 @@ if button:
 
 for section, subsections in st.session_state.json_out.items():
     for subsection, value in subsections.items():
-        response = index.query(subsection)
+        response = st.session_state.index.query(subsection)
         st.session_state.json_out[section][subsection] = response
 
 st.write(st.session_state.json_out)
