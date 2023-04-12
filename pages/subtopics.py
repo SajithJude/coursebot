23afbea9443e944b1c178e074954e40ad79bbb30
Jude Sajith <judesajith.aj@gmail.com>
2023-04-13 04:08:37 +0530

    sssss

--- pages/subtopics.py
+++ pages/subtopics.py
@@ -107,7 +107,7 @@ structure = """{
 """
 button = st.button("Generate TOC")
 if button:
-    res = st.session_state.index.query("Generate a table of contents for this document excluding objective in the following struture :\n"+ str(structure))
+    res = st.session_state.index.query("Generate a table of contents for this document excluding objective, and include only topics and subtopics in its hierarchy ")
     json_out = json.loads(res.response)
     st.session_state.json_out = json_out
     st.write(json_out)
