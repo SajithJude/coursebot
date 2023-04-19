7a9ac024193acef0972c5581da36838451ba1698
Jude Sajith <judesajith.aj@gmail.com>
2023-04-19 17:27:34 +0530

    changed string

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def delete_chapter(chapter_name):
 
 def post_xml_string(xml_string):
     url = "https://coursebot.flipick.com/couresbuilderapi/api/Course/ImportCourse"
-    data = {"ImportXML": xml_string}
+    data = {"ImportXML": str(xml_string)}
     response = requests.post(url, data=data)
     print(data)
     print(response)
