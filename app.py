7390a02fa942a7c1db4d8efee5bcce88e72d566d
Jude Sajith <judesajith.aj@gmail.com>
2023-04-19 14:06:45 +0530

    sss

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ try:
     if save_xml:
         xml_output = json_to_xml(st.session_state.new_dict, chapter_name, NoOfWordsForVOPerBullet, NoOfWordsPerBullet, NoOfBullets) 
         response = post_xml_string(xml_output)
-        if response:
+        if response is not None:
             st.info(response)
         pretty_xml = minidom.parseString(xml_output).toprettyxml()
 
