37f6f662d92c44de2cee9292199942c543d9658b
Jude Sajith <judesajith.aj@gmail.com>
2023-05-17 05:07:50 +0530

    sss

--- app.py
+++ app.py
@@ -400,7 +400,7 @@ def process_pdf(uploaded_file):
 ######################       defining tabs      ##########################################
 
 # upload_col, refine_toc,  extract_col, miss_col, edit_col,voice_col, xml_col, manage_col = st.tabs(["⚪ __Upload Chapter__","⚪ __Refine_TOC__", "⚪ __Extract_Contents__","⚪ __missing_Contents__", "⚪ __Edit Contents__", "⚪ Voice Over__", "⚪ __Export Generated XML__", "⚪ __Manage XMLs__"])
-upload_col, refine_toc,  extract_col, voice_col, xml_col = st.tabs(["⚪ __Upload Chapter__","⚪ __Refine_TOC__", "⚪ __Extract_Contents__", "⚪ __Voice Over__", "⚪ __Export Generated XML__"])
+upload_col, toc_col,  extract_col, voice_col, xml_col = st.tabs(["⚪ __Upload Chapter__","⚪ __Table Of Contents__", "⚪ __Extract_Contents__", "⚪ __Voice Over__", "⚪ __Export Generated XML__"])
 
 if "toc" not in st.session_state:
     st.session_state.toc = {}
@@ -440,7 +440,10 @@ if uploaded_file is not None:
 
 
 
-pastecol, copycol = upload_col.columns(2,gap="medium")
+
+###################### tab 2 ################
+
+pastecol, copycol = toc_col.columns(2,gap="medium")
 
 copycol.write("AI Generated TOC for unstructured documents")
 sampletoc = copycol.button("AI Generated Table")
