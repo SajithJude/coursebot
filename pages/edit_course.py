be9a27a00b5447da4cd42b51fbe91cd9ff3f37ed
Thanoraj <e18352@eng.pdn.ac.lk>
2023-05-22 18:00:27 +0530

    vffbnd

--- pages/edit_course.py
+++ pages/edit_course.py
@@ -3,7 +3,7 @@ import json
 import os
         
 
-extract_col, voice_col, xml_col = st.tabs([ "⚪ __Extract Contents__", "⚪ __XML__", "⚪ __Images__"])
+#extract_col, voice_col, xml_col = st.tabs([ "⚪ __Extract Contents__", "⚪ __XML__", "⚪ __Images__"])
 
 with open(f"output/{st.session_state.selected_pptx}", "r") as f:
     json_data = json.load(f)
