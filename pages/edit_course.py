26ab9b495f98e88d390327a4708488d3c58559b6
Thanoraj <e18352@eng.pdn.ac.lk>
2023-05-22 18:26:31 +0530

    bjgh

--- pages/edit_course.py
+++ pages/edit_course.py
@@ -2,9 +2,6 @@ import streamlit as st
 import json
 import os
         
-
-#extract_col, voice_col, xml_col = st.tabs([ "⚪ __Extract Contents__", "⚪ __XML__", "⚪ __Images__"])
-
 with open(f"output/{st.session_state.selected_pptx}", "r") as f:
     json_data = json.load(f)
     st.json(json_data)
