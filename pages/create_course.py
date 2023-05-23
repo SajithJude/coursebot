394df9819625145a5712d9141f3712da1ab003ba
Thanoraj <e18352@eng.pdn.ac.lk>
2023-05-23 11:59:45 +0530

    fkbkgbm

--- pages/create_course.py
+++ pages/create_course.py
@@ -384,26 +384,26 @@ if ecol.button("Extract and Generate"):
     
     # if "table_of_contents" in st.session_state:
     # Convert topics to new forma
-    for topic in st.session_state.table_of_contents["Topics"]:
-    
-        for topic_name, subtopics in topic.items():
-            
-            new_topic = {
-            "Topic_Name": topic_name,
-            "Subtopics": [],
-            "Topic_Summary": "",
-            "Topic_Summary_VoiceOver": ""
-            }
-
-            for subtopic in subtopics:
-                new_subtopic = {
-                    "Subtopic_Name": subtopic,
-                    "Bullets": [],
-                    "VoiceOver": [],
-                    "Image": ""
+        for topic in st.session_state.table_of_contents["Topics"]:
+        
+            for topic_name, subtopics in topic.items():
+                
+                new_topic = {
+                "Topic_Name": topic_name,
+                "Subtopics": [],
+                "Topic_Summary": "",
+                "Topic_Summary_VoiceOver": ""
                 }
-                new_topic["Subtopics"].append(new_subtopic)
-        st.session_state.dictionary["Topics"].append(new_topic)
+
+                for subtopic in subtopics:
+                    new_subtopic = {
+                        "Subtopic_Name": subtopic,
+                        "Bullets": [],
+                        "VoiceOver": [],
+                        "Image": ""
+                    }
+                    new_topic["Subtopics"].append(new_subtopic)
+            st.session_state.dictionary["Topics"].append(new_topic)
 
 
 # gen = ecol.button("Extract and Generate")
