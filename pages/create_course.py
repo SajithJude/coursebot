083825090e038e3706107095bdf957d70fa08318
Jude Sajith <judesajith.aj@gmail.com>
2023-05-25 18:17:30 +0530

    sjsjjs

--- pages/create_course.py
+++ pages/create_course.py
@@ -515,7 +515,7 @@ else:
     
     # try:
     if st.session_state.course_structure is not None:
-        modify_cs = st.text_area("Modify the structure if needed", value=st.session_state.course_structure)
+        modify_cs = st.text_area("Modify the structure if needed", value=st.session_state.course_structure,  height=600)
         if st.button("Confirm Structure"):
             convert_prompt = "Convert the following content structure into a json string, use the JSON format given bellow:\n"+ "Content Structure:\n"+ modify_cs.strip() + "\n JSON format:\n"+ str(cs_format) + ". Output should be a valid JSON string."
             json_cs = call_openai(convert_prompt)
