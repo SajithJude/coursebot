b2e8602482ffc01c4d4d498801bf345dc8ea5ce0
thananjayan91 <thananjayan1991@yahoo.com>
2023-05-29 11:09:25 +0530

    qss

--- app.py
+++ app.py
@@ -77,13 +77,13 @@ os.makedirs(upload_directory, exist_ok=True)
 m = st.markdown("""
 <style>
 div.stButton > button:first-child {
-    # background-color: #5007D9;
+    background-color: #ffffff;
     font-size:25px;border-radius:6px;
     font-color: #ffffff;
     # color: #ffffff;
     box-shadow: 1px 1px 5px grey;
     margin-top:1px;
-    min-width: 140px
+    min-width: 140px;
     border: solid #5007D9 1px;
 }
 </style>""", unsafe_allow_html=True)
