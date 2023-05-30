49297d0b1a799d7bd9a23623cf8fee090a32bdcb
Jude Sajith <judesajith.aj@gmail.com>
2023-05-30 06:48:45 +0530

    sss

--- pages/edit_video.py
+++ pages/edit_video.py
@@ -120,6 +120,7 @@ def previous_button_callback():
     global current_scene_index
     if current_scene_index > 0:
         current_scene_index -= 1
+    st.container().empty()
     update_app()
 
 # Next button callback
@@ -128,6 +129,7 @@ def next_button_callback():
     scene_count = len(scene_data["CourseStructure"]["Scenes"][0])
     if current_scene_index < scene_count - 1:
         current_scene_index += 1
+    st.container().empty()
     update_app()
 
 # Update the app based on the current scene index
