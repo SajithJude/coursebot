e5f6769f6e3ecf0133896a0801d15916fc6547a3
Jude Sajith <judesajith.aj@gmail.com>
2023-05-30 08:42:26 +0530

    sss

--- pages/edit_video.py
+++ pages/edit_video.py
@@ -169,9 +169,9 @@ with col2:
     if tabs[0]:
         # if scene:
         st.subheader(f"Scene {st.session_state.current_scene_index + 1}")
-        st.session_state.scene["Title"] = st.text_input("Title", st.session_state.scene["Title"])
-        st.session_state.scene["TextOverlay"] = st.text_input("Text Overlay", st.session_state.scene["TextOverlay"])
-        st.session_state.scene["Voiceover"] = st.text_input("Voiceover", st.session_state.scene["Voiceover"])
+        st.session_state.scene["Title"] = st.text_area("Title", st.session_state.scene["Title"])
+        st.session_state.scene["TextOverlay"] = st.text_area("Text Overlay", st.session_state.scene["TextOverlay"])
+        st.session_state.scene["Voiceover"] = st.text_area("Voiceover", st.session_state.scene["Voiceover"])
 
 # Third column - Next button and variable image display
 with col3:
