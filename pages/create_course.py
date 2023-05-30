3f1aa22d19622f0d81232350ad80739dc38bfd22
Jude Sajith <judesajith.aj@gmail.com>
2023-05-30 17:35:20 +0530

    ssss

--- pages/create_course.py
+++ pages/create_course.py
@@ -505,15 +505,15 @@ else:
     # toc_option = Uploadtab.radio("Choose a method to provide TOC", ("Generate TOC", "Copy Paste TOC"))
 
     if uploaded_file is not None:
+        # index = 
 
-            # index = 
-            if "index" not in st.session_state:
-                st.session_state.index = process_pdf(uploaded_file)
+        if "index" not in st.session_state:
+            st.session_state.index = process_pdf(uploaded_file)
 
-            # Uploadtab.success("Index created successfully")
+        # Uploadtab.success("Index created successfully")
 
-            with open(uploaded_file.name, "wb") as f:
-                f.write(uploaded_file.getbuffer())
+        with open(uploaded_file.name, "wb") as f:
+            f.write(uploaded_file.getbuffer())
     crsnm = Uploadtab.text_input("Enter Project Name")
     savnext = Uploadtab.button("Save & Proceed")
     if savnext:
